c43778b9f0ef6770ed02e523585a8f35fc4fb61f
wpgsys <132180384+wpgsys@users.noreply.github.com>
2024-08-17 02:31:54 -0500

    Create uninstall_utorrent.sh
    Basic script to uninstall annoying ass uTorrent which was on my macbook and kept running on start up...

---
+++ uninstall_utorrent.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#To Run Enter The Commands Below
+#chmod +x uninstall_utorrent.sh
+#./uninstall_utorrent.sh
+
+# Quit uTorrent if it's running
+echo "Quitting uTorrent..."
+osascript -e 'quit app "uTorrent"'
+
+# Wait a few seconds to ensure the app quits
+sleep 3
+
+# Remove the uTorrent application
+echo "Removing uTorrent application..."
+rm -rf /Applications/uTorrent.app
+
+# Remove uTorrent-related files in the Library folders
+echo "Removing uTorrent support files..."
+rm -rf ~/Library/Application\ Support/uTorrent
+rm -rf ~/Library/Preferences/com.utorrent.uTorrent.plist
+rm -rf ~/Library/Caches/com.utorrent.uTorrent
+
+# Empty the Trash (Optional)
+echo "Emptying the Trash..."
+osascript -e 'tell application "Finder" to empty the trash'
+
+# Done
+echo "uTorrent has been successfully uninstalled."
+
